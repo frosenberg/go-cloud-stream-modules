deee835337d65af7d149a6d2e8e928630fd8ee0e
Florian Rosenberg <f.rosenberg@gmail.com>
2015-11-06 00:32:29 +0100

    Fixed httpclient input URL by stripping off unwanted chars

--- go-httpclient-processor/httpclient-processor.go
+++ go-httpclient-processor/httpclient-processor.go
@@ -7,11 +7,12 @@ import (
 	"github.com/frosenberg/go-cloud-stream/stream"
 	"gopkg.in/alecthomas/kingpin.v2"
 	"io/ioutil"
+	"strings"
 )
 
 var (
 	// CLI arguments
-	url = kingpin.Flag("url", "URL of the target resource").String()
+	url = kingpin.Flag("url", "URL of the target resource").Required().String()
 	httpMethod = kingpin.Flag("httpMethod", "HTTP method, e.g., GET, POST, PUT, DELETE").Default("GET").String()
 	body = kingpin.Flag("body", "HTTP body to send along with the request").String()
 	headers = kingpin.Flag("headers", " a map of HTTP headers to send along with the request").StringMap()
@@ -21,16 +22,22 @@ var (
 )
 
 func httpclient(ch api.InputOutputChannel) {
-	log.Debugf("httpclient processor started")
+	log.Infoln("httpclient processor started")
+	log.Debugf("--url: %s", *url)
+	log.Debugf("--httpMethod: %s", *httpMethod)
+	log.Debugf("--body: %s", *body)
+	log.Debugf("--headers: %s", *headers)
+
+	trimmedUrl := strings.Trim(*url, "'\"") // strip because we have seen weird things on some shells
+
 	out := ch.Receive()
 	for {
 		msg := <-out
-
 		if (msg.Content != nil) {
 
-			req, err := http.NewRequest(*httpMethod, *url, nil)
+			req, err := http.NewRequest(*httpMethod, trimmedUrl, nil)
 			if err != nil {
-				log.Errorf("Error while creating request %s %s", *httpMethod, *url)
+				log.Errorf("Error while creating request %s %s", *httpMethod, trimmedUrl)
 				ch.Send(api.NewTextMessage([]byte(err.Error())))
 			}
 
@@ -43,16 +50,19 @@ func httpclient(ch api.InputOutputChannel) {
 
 			resp, err := client.Do(req)
 			if (err != nil) {
-				log.Errorf("Error while invoking %s %s", *httpMethod, *url)
+				log.Errorf("Error while invoking HTTP %s on %s", *httpMethod, trimmedUrl)
+				// TODO send JSON error message downstream
 				ch.Send(api.NewTextMessage([]byte(err.Error())))
+			} else {
+				body, _ := ioutil.ReadAll(resp.Body)
+				ch.Send(api.NewTextMessage(body))
 			}
-			body, _ := ioutil.ReadAll(resp.Body)
-			ch.Send(api.NewTextMessage(body))
 		}
 	}
 }
 
 func main() {
+	log.SetLevel(log.DebugLevel)
 	stream.Init()
 	stream.RunProcessor(httpclient)
 	stream.Cleanup()
